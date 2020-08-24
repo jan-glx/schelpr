c6d9a1c9048eb88d4c03457d2f4913734b51a5f6
Jan Gleixner <jan.gleixner@gmail.com>
2020-08-24 19:24:09 +0200

    fix print_encoded edge case

--- R/knitr.R
+++ R/knitr.R
@@ -4,10 +4,10 @@ print_encoded <- function(obj) {
   b <-  capture.output(a <- knitr::knit_print(obj))
   if (identical(b, character(0))) {
     if (!identical(a, obj)) {
-      cat(paste0(a, collapse="\n"))
+      cat(paste0(sprintf("%s\n", a), collapse = ""))
     }
   } else {
-    cat(paste0(b, collapse="\n"))
+    cat(paste0(sprintf("%s\n", b), collapse = ""))
   }
   invisible(obj)
 }
