54c7de8c73b8a1da11a0d26db06b48bce12e00f7
Jan Gleixner <jan.gleixner@gmail.com>
2020-11-17 10:16:47 +0100

    fully qualify functions from utils

--- R/zzz.R
+++ R/zzz.R
@@ -7,20 +7,20 @@
     attachNamespace(nsname)
     on.exit(detach(attname, character.only = TRUE))
   }
-  
+
   fam <- "NBII"
   pars <- list(zt=c(0), ot=c(1), dt=c(1,10))
   types<-list(zt="left", ot="left")
   for (name in c("zt", "ot")) {
     gen_name <- paste0(name, "_")
-    # generate truncated negative binomial 
-    capture.output(eval(expr(gamlss.tr::gen.trun(par=pars[[name]], family=!!fam, name=!!gen_name, type=types[[name]]))))
+    # generate truncated negative binomial
+    utils::capture.output(eval(expr(gamlss.tr::gen.trun(par=pars[[name]], family=!!fam, name=!!gen_name, type=types[[name]]))))
     for (f in c("p", "d", "r", "q", "")) {
       f_ori_name <- paste0(f, fam)
       f_gen_name <- paste0(f, fam, gen_name)
       f_gen_sym <- parse_expr(f_gen_name)
       f_new_name <- paste0(f, fam, name)
-      
+
       f_new_formals <- formals(f_ori_name)
       f_args <- parse_exprs(names(f_new_formals))
       names(f_args) <- names(f_new_formals)
@@ -36,7 +36,7 @@
           do.call(!!f_gen_sym, args, envir = parent.frame())
         })
         new_f})
-      assignInMyNamespace(f_new_name, new_f)
+      utils::assignInMyNamespace(f_new_name, new_f)
       #assign(f_new_name, new_f, envir= global_env())
       #if(exists(f_gen_name, envir=sys.frame(0), mode="function", inherits=FALSE))
       #  rm(list = f_gen_name, envir = sys.frame(0))
