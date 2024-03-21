7336095374f26e1944a72a2c761f3d994b4ffdd4
Jan Gleixner <jan.gleixner@gmail.com>
2024-03-21 13:58:53 +0100

    make shuffeling optionional in meta_and_reductions

--- R/seurat_helpers.R
+++ R/seurat_helpers.R
@@ -1,10 +1,10 @@
 
 #' extract and join meta information and reductions (PCA, TSNE etc) into a single, randomly ordered data.table
 #' @export
-meta_and_reductions <- function(si, max_n_dim_embeddings = 2) {
+meta_and_reductions <- function(si, max_n_dim_embeddings = 2, do_shuffle=TRUE) {
   dt <- if("cell" %in% colnames(si@meta.data)) data.table(si@meta.data) else data.table(si@meta.data, keep.rownames = "cell")
   dt <- cbind(dt, do.call(cbind, setNames(lapply(si@reductions, function(reduction) reduction@cell.embeddings[, seq_len(min(ncol(reduction@cell.embeddings), max_n_dim_embeddings))] %>% data.table), NULL)))
-  shuffle(dt)
+  if(do_shuffle) shuffle(dt) else dt
 }
 
 #' @import Seurat
